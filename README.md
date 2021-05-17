f86a23e7a4e0727cf110db20beacd7defac6bdb1
Spencer Harrison <spencerharrison0@gmail.com>
2021-05-17 10:52:58 -0400

    chapter 14 completed

--- README.md
+++ README.md
@@ -15,8 +15,8 @@ This repository will contain my progress developing a web application using the 
 - [x] Chapter 11: Facelift
 - [x] Chapter 12: Dates and Times
 - [x] Chapter 13: l18n and L10n
-- [ ] *Chapter 14: Ajax*
-- [ ] Chapter 15: A Better Application Structure
+- [x] Chapter 14: Ajax
+- [ ] *Chapter 15: A Better Application Structure*
 - [ ] Chapter 16: Full-Text Search
 - [ ] Chapter 17: Deployment on Linux
 - [ ] Chapter 18: Deployment on Heroku
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import render_template, flash, redirect, url_for, request, g
+from flask import render_template, flash, redirect, url_for, request, g, jsonify
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.urls import url_parse
 from flask_babel import _, get_locale
@@ -9,6 +9,7 @@ from app.forms import LoginForm, RegistrationForm, EditProfileForm, \
     EmptyForm, PostForm, ResetPasswordRequestForm, ResetPasswordForm
 from app.models import User, Post
 from app.email import send_password_reset_email
+from app.translate import translate
 
 @app.before_request
 def before_request():
@@ -190,3 +191,10 @@ def unfollow(username):
         return redirect(url_for('user', username=username))
     else:
         return redirect(url_for('index'))
+
+@app.route('/translate', methods=['POST'])
+@login_required
+def translate_text():
+    return jsonify({'text': translate(request.form['text'],
+                                      request.form['source_language'],
+                                      request.form['dest_language'])})
